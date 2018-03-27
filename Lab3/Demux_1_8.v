94412a1e8e6c6ab24011e9f087e00c478286a457
Dmytro <dimasakharov98@gmail.com>
2018-03-27 23:00:06 +0300

    added an 'enable' feature

--- Lab3/Demux_1_8.v
+++ Lab3/Demux_1_8.v
@@ -3,7 +3,8 @@ module Demux_1_8 (Q, EN, Addr, OutZ);
 	input [2:0]Addr;
 	output [7:0]OutZ;
 	wire Out[7:0];
- 	
+ 	//tri OutZ[7:0];
+
 	assign Out[0] =  ~Addr[0] & ~Addr[1] & ~Addr[2] & Q;
 	assign Out[1] =   Addr[0] & ~Addr[1] & ~Addr[2] & Q;
   	assign Out[2] =  ~Addr[0] &  Addr[1] & ~Addr[2] & Q;
@@ -12,6 +13,14 @@ module Demux_1_8 (Q, EN, Addr, OutZ);
 	assign Out[5] =   Addr[0] & ~Addr[1] &  Addr[2] & Q;
 	assign Out[6] =  ~Addr[0] &  Addr[1] &  Addr[2] & Q;
 	assign Out[7] =   Addr[0] &  Addr[1] &  Addr[2] & Q;
-
 	
+	assign OutZ[0] = !EN ? Out[0] : 'bz;
+	assign OutZ[1] = !EN ? Out[1] : 'bz;
+	assign OutZ[2] = !EN ? Out[2] : 'bz;
+	assign OutZ[3] = !EN ? Out[3] : 'bz;
+	assign OutZ[4] = !EN ? Out[4] : 'bz;
+	assign OutZ[5] = !EN ? Out[5] : 'bz;
+	assign OutZ[6] = !EN ? Out[6] : 'bz;
+	assign OutZ[7] = !EN ? Out[7] : 'bz; 
+
 endmodule
