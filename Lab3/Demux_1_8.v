5743519fedecf40e36ef0901ee3e811224db7bb0
Dmytro <dimasakharov98@gmail.com>
2018-03-27 22:27:45 +0300

    modified:   Lab3/Demux_1_8.v

--- Lab3/Demux_1_8.v
+++ Lab3/Demux_1_8.v
@@ -1,7 +1,17 @@
-module Demux_1_8(Q, Out, Addr, EN
-input Q, EN,
-input [2:0] Addr,
-output [7:0]Out),
-);
+module Demux_1_8 (Q, EN, Addr, OutZ);
+	input Q, EN;
+	input [2:0]Addr;
+	output [7:0]OutZ;
+	wire Out[7:0];
+ 	
+	assign Out[0] =  ~Addr[0] & ~Addr[1] & ~Addr[2] & Q;
+	assign Out[1] =   Addr[0] & ~Addr[1] & ~Addr[2] & Q;
+  	assign Out[2] =  ~Addr[0] &  Addr[1] & ~Addr[2] & Q;
+	assign Out[3] =   Addr[0] & ~Addr[1] & ~Addr[2] & Q;
+	assign Out[4] =  ~Addr[0] & ~Addr[1] &  Addr[2] & Q;
+	assign Out[5] =   Addr[0] & ~Addr[1] &  Addr[2] & Q;
+	assign Out[6] =  ~Addr[0] &  Addr[1] &  Addr[2] & Q;
+	assign Out[7] =   Addr[0] &  Addr[1] &  Addr[2] & Q;
 
-endmodule;
+	
+endmodule
