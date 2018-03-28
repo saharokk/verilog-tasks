26dcb3fed89a2d8aa537f8e8dbde4bb569004329
Dmytro <dimasakharov98@gmail.com>
2018-03-28 10:44:46 +0300

    Reduced source code

--- Lab3/Demux_1_8.v
+++ Lab3/Demux_1_8.v
@@ -11,14 +11,5 @@ module Demux_1_8 (Q, EN, Addr, Out);
 	assign Out[5] = !EN ?  Addr[0] & ~Addr[1] &  Addr[2] & Q : 'bz;
 	assign Out[6] = !EN ? ~Addr[0] &  Addr[1] &  Addr[2] & Q : 'bz;
 	assign Out[7] = !EN ?  Addr[0] &  Addr[1] &  Addr[2] & Q : 'bz;
-	
-	/*assign OutZ[0] =  Out[0] : 'bz;
-	assign OutZ[1] = !EN ? Out[1] : 'bz;
-	assign OutZ[2] = !EN ? Out[2] : 'bz;
-	assign OutZ[3] = !EN ? Out[3] : 'bz;
-	assign OutZ[4] = !EN ? Out[4] : 'bz;
-	assign OutZ[5] = !EN ? Out[5] : 'bz;
-	assign OutZ[6] = !EN ? Out[6] : 'bz;
-	assign OutZ[7] = !EN ? Out[7] : 'bz; */
 
 endmodule
